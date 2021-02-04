252753b7ebf868cfba6630814c8b3a87c93362a0
ermi13 <ermibling13@gmail.com>
2021-02-04 11:24:01 +0300

    djangoapi

---
+++ theapi/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class employees(models.Model):
+    firstname = models.CharField(max_length=15)
+    lastname=models.CharField(max_length=20)
+    employee_id = models.IntegerField()
+
+    def __str__(self):
+        return self.firstname
